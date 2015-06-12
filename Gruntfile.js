c63b8984df1ae1986d3da903eb486ca7517d5826
yeyuan <yey@agutong.com>
2015-06-12 12:38:40 +0800

    add grunt serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,6 +124,16 @@ module.exports = function(grunt) {
       }
     },
 
+    serve: {
+      options: {
+        port: 9000
+      },
+      dev: {
+        path: '<%= app.home %>',
+        //tasks: ['default', 'watch:dev']
+      }
+    }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -135,9 +145,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-serve');
 
   grunt.registerTask('lint', ['newer:jshint', 'newer:jscs']);
   grunt.registerTask('test', ['jshint', 'qunit']);
+  //grunt.registerTask('dev', ['serve:dev']);
   grunt.registerTask('dev', ['default', 'watch:dev']);
   grunt.registerTask('default', ['concat:css', 'uglify:css', 'concat', 'jade:dist', 'less:dev', 'qunit', 'uglify']);
 
